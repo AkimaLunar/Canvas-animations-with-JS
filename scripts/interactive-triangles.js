91aa77a5ca43780568639656bcd7d85a74f08435
Ria Nicole Carmin <akimalunar@gmail.com>
2018-01-29 11:29:26 -0800

    interactive triangles

--- scripts/interactive-triangles.js
+++ scripts/interactive-triangles.js
@@ -1,55 +1,132 @@
 (function () {
-  var canvas = document.getElementById('canvas');
-  var canvasWidth = document.getElementById('main').offsetWidth;
-  var canvasHeight = window.innerHeight - document.getElementById('navigation').offsetHeight;
-  var canvasCenter = {
+  const canvas = document.getElementById('canvas');
+  const canvasWidth = document.getElementById('main').offsetWidth;
+  const canvasHeight = window.innerHeight - document.getElementById('navigation').offsetHeight;
+  const canvasCenter = {
     x: canvasWidth / 2,
     y: canvasHeight / 2
   };
+
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
-  var maxSize = 20;
-  var mouse = {
+  const maxSize = 20;
+
+  let mouse = {
     x: undefined,
     y: undefined
   }
 
-  var c = canvas.getContext('2d');
+  const c = canvas.getContext('2d');
 
-  function Triangle(x, y, dx, dy, maxSize) {
-    this.x = x;
-    this.y = y;
-    this.left = 10 + Math.random() * (maxSize - 10);
-    this.right = 1 + Math.random() * (maxSize - 10);
-    this.dx = dx;
-    this.dy = dy;
-    this.draw = function () {
+  window.addEventListener('mousemove', function (e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+  })
+
+  window.addEventListener('mouseout', function (e) {
+    mouse.x = undefined;
+    mouse.y = undefined;
+  })
+
+  class Shape {
+    constructor(x, y, dx, dy, size) {
+      this.x = x;
+      this.y = y;
+      this.dx = dx;
+      this.dy = dy;
+      this.size = size;
+      this.maxSize = size * 8;
+      this.minSize = size;
+    }
+
+    draw() {
       c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.fill();
+    }
+
+    update() {
+      if (this.x + this.size > canvasWidth || this.x - this.size < 0) {
+        this.dx = -this.dx;
+      }
+      if (this.y + this.size > canvasHeight || this.y - this.size < 0) {
+        this.dy = -this.dy;
+      }
+      this.x += this.dx;
+      this.y += this.dy;
+
+      if (
+        mouse.x - this.x < 50 &&
+        mouse.x - this.x > -50 &&
+        mouse.y - this.y < 50 &&
+        mouse.y - this.y > -50 &&
+        this.size < this.maxSize
+      ) {
+        this.size += 1;
+        console.log(this.size);
+      } else if (this.size > this.minSize) {
+        // console.log(`${mouse.x} ${this.x}`)
+        // this.radius -= 1;
+      }
+
+      this.draw();
+    }
+
+  }
+
+  class Dot extends Shape {
+    constructor(x, y, dx, dy, size) {
+      super(x, y, dx, dy, size);
+      this.radius = this.size / 8;
+    }
+    draw() {
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
       c.fillStyle = 'rgb(0, 70, 183)';
+      c.fill();
+    }
+  }
+
+  class Triangle extends Shape {
+    constructor(x, y, dx, dy, size) {
+      super(x, y, dx, dy, size);
+      this.left = 4 + Math.random() * (this.size - 10);
+      this.right = 1 + Math.random() * (this.size - 10);
+    }
+
+    draw() {
+      c.beginPath();
+      c.fillStyle = 'rgba(109, 0, 183,.15)';
       c.moveTo(this.x, this.y);
       c.lineTo(this.x + this.left, this.y - this.left);
       c.lineTo(this.x + this.left, this.y + this.right);
       c.lineTo(this.x, this.y);
       c.fill();
     }
-
-    this.update = function () {
-      this.x += this.dx;
-      this.y += this.dy;
-      this.draw();
-    }
   }
   var triangleArray = [];
-  for (var i = 1; i < 60; i++) {
-    var x = Math.random() * canvasWidth - maxSize;
-    var y = Math.random() * canvasHeight - maxSize;
-    var dx = 1 + (Math.random() + -0.5) * 4;
-    var dy = 1 + (Math.random() + -0.5) * 4;
+  for (var i = 1; i < 160; i++) {
+    var x = Math.random() * (canvasWidth - maxSize * 2) + maxSize;
+    var y = Math.random() * (canvasHeight - maxSize * 2) + maxSize;
+    var dx = (Math.random() + -0.5) * 4;
+    var dy = (Math.random() + -0.5) * 4;
     triangleArray.push(new Triangle(x, y, dx, dy, maxSize));
   }
 
   for (let i = 0; i < triangleArray.length; i++) {
     triangleArray[i].draw();
+  }
+  var dotArray = [];
+  for (var i = 1; i < 760; i++) {
+    var x = Math.random() * (canvasWidth - maxSize * 2) + maxSize;
+    var y = Math.random() * (canvasWidth - maxSize * 2) + maxSize;
+    var dx = Math.random() + -0.5;
+    var dy = Math.random() + -0.5;
+    dotArray.push(new Dot(x, y, dx, dy, maxSize));
+  }
+
+  for (let i = 0; i < triangleArray.length; i++) {
+    dotArray[i].draw();
   }
 
   function animate() {
@@ -58,7 +135,46 @@
     for (let i = 0; i < triangleArray.length; i++) {
       triangleArray[i].update();
     }
+    for (let i = 0; i < dotArray.length; i++) {
+      dotArray[i].update();
+    }
   }
 
   animate();
 })();
+
+// export default class InteractiveTriangles {
+//   constructor(c, amount, maxSize) {
+//     this.c = c;
+//     this.amount = amount;
+//     this.maxSize = maxSize;
+//   }
+
+//   generate(Shape) {
+//     let shapeArray = [];
+//     var x = Math.random() * canvasWidth - maxSize;
+//     var y = Math.random() * canvasHeight - maxSize;
+//     var dx = 1 + (Math.random() + -0.5) * 4;
+//     var dy = 1 + (Math.random() + -0.5) * 4;
+//     shapeArray.push(new Shape(x, y, dx, dy, maxSize))
+//     return shapeArray;
+//   }
+
+//   init(...shapes) {
+//     [...shapes].forEach(shapeArray => {
+//       for (let i = 0; i < shapeArray.length; i++) {
+//         shapeArray[i].draw();
+//       }
+//     })
+//   }
+
+//   animate(...shapes) {
+//     requestAnimationFrame(this.animate);
+//     c.clearRect(0, 0, canvasWidth, canvasHeight);
+//     [...shapes].forEach(shapeArray => {
+//       for (let i = 0; i < shapeArray.length; i++) {
+//         shapeArray[i].update();
+//       }
+//     })
+//   }
+// }
